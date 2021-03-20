e267b50a34ac83031ad5ae6da76a957ef4ad7679
Juzzelmae <delrosariojuzzelmae@gmail.com>
2021-03-20 18:38:26 +0800

    Method 3 and 4

--- method3and4.java
+++ method3and4.java
@@ -1,1 +1,13 @@
-Del Rosario
+public class Methods {
+     //JUZZEL MAE DEL ROSARIO
+static void Method3() {
+    System.out.println("Del Rosario, Juzzel Mae P.");
+  }
+  static void Method4() {
+    System.out.println("MEMBER");
+  }
+  public static void main(String[] args) {
+    Method3();
+    Method4(); 
+  }
+}
